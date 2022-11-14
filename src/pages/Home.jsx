255b30cc95edcd10b578667e353c0a84c471b3f1
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-11-14 20:46:27 +0200

    deb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
-
 import { Layout } from '../layouts';
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const count = useSelector(({ counter }) => {
-    return counter;
-  });
-
   return (
     <Layout>
       <div className="container mx-auto px-4"></div>
